1684b5177271fa73ceefa72b2728b8aef57b00de
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-09-21 12:13:31 +0300

    added files for docker support

--- src/GeekComparer.Web/Program.cs
+++ src/GeekComparer.Web/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddHealthChecks();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
     opt => opt.UseNpgsql(
@@ -14,17 +15,45 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     )
 );
 
+/*
+// configuration for https
+// https://learn.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-7.0&tabs=visual-studio%2Clinux-ubuntu#options
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpsRedirection(
+        opt =>
+        {
+            opt.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+            opt.HttpsPort = 443;
+        }
+    );
+
+    builder.Services.AddHsts(
+        opt =>
+        {
+            opt.Preload = true;
+            opt.IncludeSubDomains = true;
+            opt.MaxAge = TimeSpan.FromMinutes(1);
+        }
+    );
+}*/
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
@@ -32,15 +61,19 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/"
 );
+app.MapHealthChecks("/healthz");
 
-using var context = app.Services.CreateScope()
-   .ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-if (!context.Database.EnsureCreated())
+if (!app.Environment.IsProduction())
 {
-    context.Database.Migrate();
-    context.Seed();
-    context.SaveChanges();
+    using var context = app.Services.CreateScope()
+       .ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    if (!context.Database.CanConnect())
+    {
+        context.Database.Migrate();
+        context.Seed();
+        context.SaveChanges();
+    }
 }
 
 app.Run();
